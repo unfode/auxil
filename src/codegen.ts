d377bc9b03019e77faf410936760405e4d7dfb90
unfode <forrest.futao.wei@gmail.com>
2025-07-10 20:50:33 -0700

    Add constructor maker functions.

--- src/codegen.ts
+++ src/codegen.ts
@@ -4,9 +4,12 @@ function nullary_constructor(name: string): string {
 
 function sum_type_internal(type_name: string, constructor_names: string[]): string {
   const constructors = constructor_names.map(nullary_constructor)
-  const union = constructors.map(constructor => `| ${constructor}`).join('\n')
+  const union = constructors.map(constructor => `| ${constructor}\n`).join('')
 
-  return `type ${type_name} =\n${union}\n`
+  const constructor_maker = (name: string): string => `function ${name}(): ${type_name} {\n  return {kind: '${name}'}\n}\n`
+  const constructor_makers = constructor_names.map(constructor_maker).join('')
+
+  return `type ${type_name} =\n${union}${constructor_makers}\n`
 }
 
 function sum_type(type_name: string, constructor_names: string[]): void {
