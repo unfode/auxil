918aa721fb82d4b0d99b021d0b84f75070653b60
unfode <forrest.futao.wei@gmail.com>
2025-07-26 17:35:56 -0700

    Fix Git tag.

--- src/release.ts
+++ src/release.ts
@@ -3,7 +3,7 @@ import { execSync } from "child_process";
 function minor(token: string): void {
   const commands = [
     "npx tsc",
-    'npm version minor --message "%s"',
+    'npm version minor --tag-version-prefix="" --message "%s"',
     `npm publish --//registry.npmjs.org/:_authToken=${token}`,
   ];
   const output = execSync(commands.join(" && "));
