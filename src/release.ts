6a89c0ecd4e123bb3e26a3fc7ed47170ae9b401f
unfode <forrest.futao.wei@gmail.com>
2025-07-26 17:25:32 -0700

    Fix bug.

--- src/release.ts
+++ src/release.ts
@@ -2,9 +2,9 @@ import { execSync } from "child_process";
 
 function minor(token: string): void {
   const commands = [
-    "npx run tsc",
+    "npx tsc",
     'npm version minor --message "%s"',
-    `npm publish //registry.npmjs.org/:_authToken=${token}`,
+    `npm publish --//registry.npmjs.org/:_authToken=${token}`,
   ];
   const output = execSync(commands.join(" && "));
   console.log(output.toString());
